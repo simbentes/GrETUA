cad8af9618b0d8e94d3d22cce52846aa2073c723
simbentes <sdgbentes@gmail.com>
2021-07-07 22:18:19 +0100

    memorias implementadas

--- components/cp_memorias.php
+++ components/cp_memorias.php
@@ -11,16 +11,15 @@
                 $link = new_db_connection();
                 $stmt = mysqli_stmt_init($link);
 
-                $query = "SELECT eventos.id_eventos, DATE(MIN(data_eventos.data)), DATE_FORMAT(TIME(MIN(data_eventos.data)), '%H:%i'), eventos.nome, fotos_eventos.foto, tipo_eventos.nome FROM eventos
+                $query = "SELECT eventos.id_eventos, DATE(data_eventos.data), eventos.nome, fotos_eventos.foto FROM eventos
 INNER JOIN data_eventos
 ON data_eventos.ref_id_eventos = eventos.id_eventos
-INNER JOIN guardados_vou
-ON eventos.id_eventos = guardados_vou.ref_id_eventos
 INNER JOIN fotos_eventos
 ON fotos_eventos.ref_id_eventos = eventos.id_eventos
 INNER JOIN tipo_eventos
 ON tipo_eventos.id_tipo_eventos = eventos.ref_id_tipo_eventos
-WHERE guardados_vou.ref_id_utilizadores = 1 AND fotos_eventos.capa = 1 AND data_eventos.data > NOW()
+WHERE fotos_eventos.capa = 1 AND data_eventos.data < NOW()
+GROUP BY eventos.id_eventos
 ORDER BY data_eventos.data;";
 
                 if (mysqli_stmt_prepare($stmt, $query)) {
@@ -29,7 +28,7 @@ ORDER BY data_eventos.data;";
                     mysqli_stmt_execute($stmt);
 
                     /* bind result variables */
-                    mysqli_stmt_bind_result($stmt, $id_evento, $data_evento, $hora_evento, $nome_evento, $foto, $tipo);
+                    mysqli_stmt_bind_result($stmt, $id_evento, $data_evento, $nome_evento, $foto);
 
                     mysqli_stmt_store_result($stmt);
 
@@ -38,61 +37,9 @@ ORDER BY data_eventos.data;";
                     } else {
                         while (mysqli_stmt_fetch($stmt)) {
 
-                            $hora_h_evento = str_replace(":", "h", $hora_evento);
 
                             ?>
 
-                            <div class="memoria swiper-slide">
-                                <a href="memoria.php?id=<?= $id_evento ?>">
-
-                                    <img class="img-memoria" src="img/<?= $foto ?>">
-                                    <div class="desc-memoria container-fluid">
-                                        <h3 class="top-right"><?= $nome_evento ?></h3>
-                                        <div class="row">
-                                            <p class="text-cinza desc-curta">A habitual e muito rica aliança com o Núcleo de
-                                                Estudantes de Música - AAUAv
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="bola">
-                                        <svg height="26" width="26" viewBox="0 0 100 100">
-                                            <circle cx="50" cy="50" r="40" fill="white"/>
-                                        </svg>
-                                    </div>
-                                    <div class="ano">
-                                        <h1 class="text-center mb-0">
-                                            <?= $data_evento ?>
-                                        </h1>
-                                    </div>
-
-                                </a>
-                            </div>
-
-                            <div class="memoria swiper-slide">
-                                <a href="memoria.php?id=<?= $id_evento ?>">
-
-                                    <img class="img-memoria" src="img/<?= $foto ?>">
-                                    <div class="desc-memoria container-fluid">
-                                        <h3 class="top-right"><?= $nome_evento ?></h3>
-                                        <div class="row">
-                                            <p class="text-cinza desc-curta">A habitual e muito rica aliança com o Núcleo de
-                                                Estudantes de Música - AAUAv
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="bola">
-                                        <svg height="26" width="26" viewBox="0 0 100 100">
-                                            <circle cx="50" cy="50" r="40" fill="white"/>
-                                        </svg>
-                                    </div>
-                                    <div class="ano">
-                                        <h1 class="text-center mb-0">
-                                            <?= $data_evento ?>
-                                        </h1>
-                                    </div>
-
-                                </a>
-                            </div>
                             <div class="memoria swiper-slide">
                                 <a href="memoria.php?id=<?= $id_evento ?>">
 
