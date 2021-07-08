9617d53eff9cf764e865214f689f6dd626b7a1fc
simbentes <sdgbentes@gmail.com>
2021-07-08 20:42:35 +0100

    favicon

--- helpers/help_link.php
+++ helpers/help_link.php
@@ -1,5 +1,5 @@
-<link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png">
-<link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
       rel="stylesheet">
