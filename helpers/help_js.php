84fa3d517fb21a394f3e5e61660c14cbd07ad708
simbentes <sdgbentes@gmail.com>
2021-07-12 16:25:14 +0100

    artista

--- helpers/help_js.php
+++ helpers/help_js.php
@@ -1,4 +1,4 @@
 <script src="js/swiper-bundle.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/script.js"></script>
-<script src="js/main.js"></script>
+<script src="js/app.js"></script>
--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-// Check that service workers are supported
 if ('serviceWorker' in navigator) {
     // Use the window load event to keep the page load performant
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/deca_20L4/deca_20L4_03/sw.js').then(registration => {
+        navigator.serviceWorker.register('/deca_20L4/deca_20L4_33/sw.js').then(registration => {
             console.log("sw reigstrao!!")
             console.log(registration)
         }).catch(error=>{
