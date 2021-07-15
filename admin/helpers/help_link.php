70307e86f197a16ba13f67cb047e5f1be7351288
simbentes <sdgbentes@gmail.com>
2021-07-15 02:26:55 +0100

    oi

--- admin/helpers/help_link.php
+++ admin/helpers/help_link.php
@@ -1,8 +1,7 @@
 <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png">
 <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png">
-<link href="../fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
       rel="stylesheet">
 <link href="css/sb-admin-2.min.css" rel="stylesheet">
 <link href="css/estilos.css" rel="stylesheet">
-
+<script src="https://kit.fontawesome.com/290793a447.js" crossorigin="anonymous"></script>
