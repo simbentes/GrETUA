4865e82ca0d8ccbc13994bbe99274ff812eb94fb
simbentes <sdgbentes@gmail.com>
2021-07-21 23:18:41 +0100

    commit final

--- components/cp_reservar_success.php
+++ components/cp_reservar_success.php
@@ -31,7 +31,7 @@ WHERE data_eventos.id_data_eventos = ? AND reservas.ref_id_utilizadores = " . $_
                 <section class="success-container">
                     <div>
                         <h3>Reserva efetuada!</h3>
-                        <a href="https://www.google.com/calendar/render?action=TEMPLATE&text=<?= htmlspecialchars($nome_evento) ?>&dates=<?= htmlspecialchars($data_qs) ?>/<?= htmlspecialchars($data_fim_qs) ?>&details=<?= htmlspecialchars($descricao_qs) ?>&location=GrETUA,+3810-502+Aveiro,+Portugal&sf=true&output=xml"
+                        <a href="https://www.google.com/calendar/render?action=TEMPLATE&text=<?= htmlspecialchars($nome_evento) ?>&dates=<?= htmlspecialchars($data_qs) ?>/<?= htmlspecialchars($data_qs) ?>&details=<?= htmlspecialchars($descricao_qs) ?>&location=GrETUA,+3810-502+Aveiro,+Portugal&sf=true&output=xml"
                            class="btn btn-grande btn-google w-75"><img
                                     src="img/g_calendar.svg" class="logo">Adicionar ao Google Calendar
                         </a>
